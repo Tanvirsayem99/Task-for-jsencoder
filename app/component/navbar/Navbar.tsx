2f5665c38d59aa6a06a50ef51088e548bdc6d520
Tanvir Haider Sayem <tanvirhaider292297@gmail.com>
2024-05-11 08:39:18 -0700

    implement navbar

---
+++ app/component/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import defaultImage from '@/public/default.jpeg'
+import "./navbar.css";
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <h1>Hotel Haven</h1>
+            <div className='navlinks'>
+                <Link href={'/'}>Sign In</Link>
+                <Link href={'/'}>Sign UP</Link>
+                <Link href={'/'}></Link>
+                
+            </div>
+            <div className='users'>
+                <Image className='img' src={defaultImage} alt='default' />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
